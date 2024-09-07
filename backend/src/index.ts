f337bec5959ebbedf150d43671fd0475984ac3c2
imran-binhasan <imranbinhasan.work@gmail.com>
2024-09-07 10:00:00 +0600

    Add database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,9 @@ import express, {Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config"
 
+import mongoose from "mongoose"
 
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string)
 
 const app = express();
 app.use(express.json());
